3ae5b41e9172d3e637fd1d20ca22d180d16631b6
HimalN <himalnimsara88@gmail.com>
2024-11-08 01:52:46 +0530

    Initial commit

---
+++ Green-shadow-crop-management-backend/src/main/java/lk/ijse/Green_shadow_crop_management_backend/GreenShadowCropManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Green_shadow_crop_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowCropManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShadowCropManagementBackendApplication.class, args);
+	}
+
+}
---
+++ Green-shadow-crop-management-backend/src/test/java/lk/ijse/Green_shadow_crop_management_backend/GreenShadowCropManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Green_shadow_crop_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowCropManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
