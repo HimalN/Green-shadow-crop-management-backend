330b69a32d20b7e7994c6a2280746368a62abb3f
HimalN <himalnimsara88@gmail.com>
2024-12-06 00:01:34 +0530

    -->Manage Staff

--- Green-shadow-crop-management-backend/src/main/java/lk/ijse/Green_shadow_crop_management_backend/service/impl/StaffServiceIMPL.java
+++ Green-shadow-crop-management-backend/src/main/java/lk/ijse/Green_shadow_crop_management_backend/service/impl/StaffServiceIMPL.java
@@ -1,9 +1,14 @@
 package lk.ijse.Green_shadow_crop_management_backend.service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.Green_shadow_crop_management_backend.customStatusCodes.SelectedStaffErrorStatus;
+import lk.ijse.Green_shadow_crop_management_backend.dao.StaffDAO;
 import lk.ijse.Green_shadow_crop_management_backend.dto.StaffStatus;
 import lk.ijse.Green_shadow_crop_management_backend.dto.impl.StaffDTO;
+import lk.ijse.Green_shadow_crop_management_backend.entity.impl.Staff;
 import lk.ijse.Green_shadow_crop_management_backend.service.StaffService;
+import lk.ijse.Green_shadow_crop_management_backend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +17,33 @@ import java.util.List;
 @Transactional
 @Service
 public class StaffServiceIMPL implements StaffService {
+    @Autowired
+    private StaffDAO staffDAO;
+
+    @Autowired
+    private Mapping staffMapping;
+
     @Override
     public void saveStaff(StaffDTO staffDTO) {
-
+        Staff saveField = staffDAO.save(staffMapping.toStaffEntity(staffDTO));
+        if(saveField == null){
+            throw new RuntimeException("Staff not saved");
+        }
     }
 
     @Override
     public List<StaffDTO> getAllStaffs() {
-        return List.of();
+        return staffMapping.asStaffDTOList(staffDAO.findAll());
     }
 
     @Override
     public StaffStatus getStaff(String staffId) {
-        return null;
+        if(staffDAO.existsById(staffId)){
+            var selectedStaff = staffDAO.getReferenceById(staffId);
+            return staffMapping.toStaffDTO(selectedStaff);
+        }else {
+            return new SelectedStaffErrorStatus(404,"Selected staff not found");
+        }
     }
 
     @Override
